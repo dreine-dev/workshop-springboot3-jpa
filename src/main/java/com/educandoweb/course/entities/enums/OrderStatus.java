a00da7c872d63cf69c02f89b95aa6b439f9265d4
Dreine Antonio <dreineantonio@hotmail.com>
2025-10-13 17:19:04 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -2,18 +2,22 @@ package com.educandoweb.course.entities.enums;
 
 public enum OrderStatus {
 
-	WAITING_PAYMENT(1), PAID(2), SHIPPED(3), DELIVERED(4), CANCELAD(5);
+	WAITING_PAYMENT(1), PAID(2), SHIPPED(3),  DELIVERED(4), CANCELAD(5);
 
+	//codigo do tipo enumerado exemplo:1
 	private int code;
 
+	//construtor
 	private OrderStatus(int code) {
 		this.code = code;
 	}
 	
+	//metodo pra tornar o construtor com acesso publico
 	public int getCode() {
 		return code;
 	}
 	
+	//percorre os tipos enumerados
 	public static OrderStatus valueOf(int code) {
 		for(OrderStatus value : OrderStatus.values()) {
 			if(value.getCode() == code) {
