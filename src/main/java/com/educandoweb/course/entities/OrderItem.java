7d8132b4de32d010075e34096d6efe13c4d25981
Dreine Antonio <dreineantonio@hotmail.com>
2025-10-29 11:44:34 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -4,11 +4,12 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.course.entities.pk.OrderItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_order_item")
@@ -24,28 +25,30 @@ public class OrderItem implements Serializable {
 	public OrderItem() {
 	}
 
+
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
-		id.setOrder(order);
-		id.setProduct(product);
+		this.id = new OrderItemPk();
+		this.id.setOrder(order);
+		this.id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
-
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
 
-	public void setProduct(Product product) {
-		id.setProduct(product);
+	public void setOrder(Order order) {
+		id.setOrder(order);
 	}
 
 	public Product getProduct() {
 		return id.getProduct();
 	}
 
-	public void setOrder(Order order) {
-		id.setOrder(order);
+	public void setProduct(Product product) {
+		id.setProduct(product);
 	}
 
 	public Integer getQuantity() {
--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -15,13 +15,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-	private static final Long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -30,7 +29,7 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	
+
 	@ManyToMany
 	@JoinTable(name = "tb_product_category",
 	joinColumns = @JoinColumn(name = "product_id"),
